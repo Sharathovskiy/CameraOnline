2586fb247f81989ca52a22d140cffd2250ac8321
Mateusz Szarata <mateusz.szarata@op.pl>
2017-08-21 15:45:27 +0200

    Taking photos is possible now, photo displays next to camera.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,19 +5,18 @@
 
     <div class="row ">
         <h1>Take a picture of yourself!</h1>
-    </div>
-    
-    <div id="camera_info" class="row">
-        <div id="camera"></div> <br>
-        <button id="take_snapshots" class="btn btn-success btn-sm">Take Snapshots</button>
+        <video id="video" width="400" height="300" autoplay></video>
+        <canvas id="canvas" width="400" height="300"></canvas>
     </div>
     
     <div class="row">
-        <form>
-            <input tpye="button" value="Take picture!" class="btn btn-success btn-sm" onClick="take_snapshot()">
-        </form>
+        <button id="snap" onClick="snap()">Snap photo</button>
     </div>
     
-    
 </div>
+
+@endsection
+
+@section('scripts')
+<script src="{{asset("js/webcam.js")}}"></script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,5 +17,6 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
+    @yield('scripts')   
 </body>
 </html>
